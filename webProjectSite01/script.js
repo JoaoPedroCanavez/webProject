f4a97ea507b2bccde07e5360c8c876a3eb0fa8f3
João Pedro <jsantossilvano@gmail.com>
2023-09-24 11:03:38 -0300

    changed project

--- webProjectSite01/script.js
+++ webProjectSite01/script.js
@@ -11,4 +11,39 @@ var swiper = new Swiper(".swiper", {
   keyboard: true,
 });
 
+let nome = document.querySelector("#nome")
 
+
+
+const campoQtd = document.querySelector("#qtde");
+const output = document.querySelector("output")
+const campoPrazo = document.querySelector("#prazo")
+const campoJs = document.querySelector("#js")
+campoJs.addEventListener("change", calcular)
+
+
+campoPrazo.addEventListener("input",   function() {
+    
+  const labelPrazo = document.querySelector("label[for = 'prazo']")
+
+  labelPrazo.innerHTML = `Quanto gostou :  ${campoPrazo.value} %`
+  calcular()
+})
+
+
+campoQtd.addEventListener("change" , calcular )
+
+function calcular(){
+
+console.log("Calcular Estrelas")
+let qtde = campoQtd.value
+let valor = qtde * 1;
+
+if(campoJs.checked){
+
+  alert("Voce recebera o alerta no email")
+  
+  }
+
+output.innerHTML=  `${valor} Estrelas`
+}
